d89d43a2e171cafe1a3d9b735e4ea8761a50a596
indramhaarta <indramhrt@gmail.com>
2023-04-21 01:59:25 +0700

    Add 6 name

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -31,6 +31,21 @@ const Home = (props) => {
           <div className={classes["home-left__title"]}>
             Hi, i'm <span>Indra Mahakwasa</span>
           </div>
+          <div className={classes["home-left__title"]}>
+            Hi, i'm <span>Indra Mahakwasa</span>
+          </div>
+          <div className={classes["home-left__title"]}>
+            Hi, i'm <span>Indra Mahakwasa</span>
+          </div>
+          <div className={classes["home-left__title"]}>
+            Hi, i'm <span>Indra Mahakwasa</span>
+          </div>
+          <div className={classes["home-left__title"]}>
+            Hi, i'm <span>Indra Mahakwasa</span>
+          </div>
+          <div className={classes["home-left__title"]}>
+            Hi, i'm <span>Indra Mahakwasa</span>
+          </div>
           <div className={classes["home-left__sub-title"]}>
             <span>Computer Science</span> Student at University of Indonesia
           </div>
